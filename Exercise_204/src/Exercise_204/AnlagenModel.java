ae2faa42a21e20285426b127f06b0c45b836bebc
heralc15\Alex <rednax.herbst@gmail.com>
2018-10-09 09:51:50 +0200

    Model class added

---
+++ Exercise_204/src/Exercise_204/AnlagenModel.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercise_204;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Alex
+ */
+public class AnlagenModel extends AbstractTableModel{
+    private ArrayList<Anlage> list = new ArrayList<>();
+    
+    private String[] colNames = {"Bezeichnung", "AK", "Inbetriebnahme", "ND", "bish. ND", "Afa bisher", "Wert vor Afa", "Afa d.J.", "BW 31.12."};
+    
+    @Override
+    public int getRowCount() {
+        return list.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colNames.length;
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        Anlage anlage = list.get(i);
+        return anlage;
+    }
+    
+}
